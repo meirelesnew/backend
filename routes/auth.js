761b7e27e4b95a1ad3c4d0abdbe7a79cc47ec896
meirelesnew <muty.bjj@gmail.com>
2026-04-24 15:52:11 -0300

    fix: routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,8 @@
-const express = require("express");
-const router  = express.Router();
-const auth    = require("../controllers/authController");
-const { autenticar } = require("../middleware/auth");
-
-router.post("/register",        auth.register);
-router.post("/login",           auth.login);
-router.post("/recuperar-senha", auth.solicitarRecuperacao);
-router.post("/redefinir-senha", auth.redefinirSenha);
-router.post("/confirmar",       auth.confirmarConta);
-router.post("/reenviar",        auth.reenviarConfirmacao);
-router.get ("/perfil",          autenticar, auth.perfil);
-
-module.exports = router;
+const r=require("express").Router(),a=require("../controllers/authController"),{autenticar}=require("../middleware/auth");
+r.post("/register",a.register);
+r.post("/login",a.login);
+r.post("/google",a.googleAuth);
+r.post("/recuperar-senha",a.solicitarRecuperacao);
+r.get("/perfil",autenticar,a.perfil);
+r.put("/perfil",autenticar,a.atualizarPerfil);
+module.exports=r;
