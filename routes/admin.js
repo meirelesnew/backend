14ef9b8e9e5d57794e7c78f7f55b2230e522936c
meirelesnew <muty.bjj@gmail.com>
2026-04-24 15:52:18 -0300

    fix: routes/admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,6 @@
-const express = require("express");
-const router  = express.Router();
-const admin   = require("../controllers/adminController");
-const { autenticarAdmin } = require("../middleware/auth");
-
-router.get   ("/usuarios",       autenticarAdmin, admin.listarUsuarios);
-router.get   ("/usuarios/:id",   autenticarAdmin, admin.buscarUsuario);
-router.put   ("/usuarios/:id",   autenticarAdmin, admin.atualizarUsuario);
-router.delete("/usuarios/:id",   autenticarAdmin, admin.deletarUsuario);
-router.get   ("/stats",          autenticarAdmin, admin.estatisticas);
-
-module.exports = router;
+const r=require("express").Router(),a=require("../controllers/adminController"),{autenticarAdmin}=require("../middleware/auth");
+r.get("/usuarios",autenticarAdmin,a.listarUsuarios);
+r.post("/promover",autenticarAdmin,a.promoverAdmin);
+r.get("/stats",autenticarAdmin,a.getStats);
+r.delete("/usuario/:uid",autenticarAdmin,a.deletarUsuario);
+module.exports=r;
